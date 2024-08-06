3c2717f2cce3e0b2a6fa5efc3b509d54c8778830
Larisagabriela329 <larisagabriela329@yahoo.com>
2024-08-06 14:20:58 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const button = document.querySelector(".order-button");
+
+const backdrop = document.querySelector(".modal-backdrop");
+
+button.addEventListener("click", toggleModal);
+
+backdrop.addEventListener("click", (e) => {
+
+  const target = e.target.closest(".close-button");
+
+  if (!target) {
+
+    return;
+
+  }
+
+  toggleModal();
+
+});
+
+function toggleModal() {
+
+  backdrop.classList.toggle("is-open");
+
+}
